137d30f91cb542a2fefeceb984e58d53786b0f27
yowcow <yowcow@gmail.com>
2015-03-17 12:15:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Amazon::SQS::Worker - It's new $module
+
+# SYNOPSIS
+
+    use Amazon::SQS::Worker;
+
+# DESCRIPTION
+
+Amazon::SQS::Worker is ...
+
+# LICENSE
+
+Copyright (C) yowcow.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+yowcow <yowcow@gmail.com>
---
+++ lib/Amazon/SQS/Worker.pm
@@ -0,0 +1,39 @@
+package Amazon::SQS::Worker;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Amazon::SQS::Worker - It's new $module
+
+=head1 SYNOPSIS
+
+    use Amazon::SQS::Worker;
+
+=head1 DESCRIPTION
+
+Amazon::SQS::Worker is ...
+
+=head1 LICENSE
+
+Copyright (C) yowcow.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+yowcow E<lt>yowcow@gmail.comE<gt>
+
+=cut
+
